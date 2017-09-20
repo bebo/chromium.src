aa34a97a3e157806f9ba7ace47c95e8f9932834e
Jake Loo <slow-@live.com>
2017-09-19 17:55:56 -0700

    Support direct show filter category based on whitelist for bebo game capture

--- media/capture/video/win/video_capture_device_factory_win.cc
+++ media/capture/video/win/video_capture_device_factory_win.cc
@@ -63,6 +63,17 @@ static_assert(arraysize(kBlacklistedCameraNames) == BLACKLISTED_CAMERA_MAX + 1,
               "kBlacklistedCameraNames should be same size as "
               "BlacklistedCameraNames enum");
 
+enum WhitelistedFilterNames {
+  WHITELISTED_FILTER_BEBO_CAPTURE = 0,
+  WHITELISTED_FILTER_MAX = WHITELISTED_FILTER_BEBO_CAPTURE,
+};
+static const char* const kWhitelistedFilterNames[] = {
+  "bebo-game-capture",
+};
+static_assert(arraysize(kWhitelistedFilterNames) == WHITELISTED_FILTER_MAX + 1,
+              "kWhitelistedFilterNames should be same size as "
+              "WhitelistedFilterNames enum");
+
 static bool LoadMediaFoundationDlls() {
   static const wchar_t* const kMfDLLs[] = {
       L"%WINDIR%\\system32\\mf.dll",
@@ -132,6 +143,19 @@ static bool IsDeviceBlackListed(const std::string& name) {
   return false;
 }
 
+static bool IsDeviceWhiteListed(const std::string& name) {
+  DCHECK_EQ(WHITELISTED_FILTER_MAX + 1,
+            static_cast<int>(arraysize(kWhitelistedFilterNames)));
+  for (size_t i = 0; i < arraysize(kWhitelistedFilterNames); ++i) {
+    if (base::StartsWith(name, kWhitelistedFilterNames[i],
+                         base::CompareCase::INSENSITIVE_ASCII) != 0) {
+      DVLOG(1) << "Enumerated whitelist device: " << name;
+      return true;
+    }
+  }
+  return false;
+}
+
 static std::string GetDeviceModelId(const std::string& device_id) {
   const size_t vid_prefix_size = sizeof(kVidPrefix) - 1;
   const size_t pid_prefix_size = sizeof(kPidPrefix) - 1;
@@ -167,13 +191,59 @@ static void GetDeviceDescriptorsDirectShow(Descriptors* device_descriptors) {
                                        enum_moniker.GetAddressOf(), 0);
   // CreateClassEnumerator returns S_FALSE on some Windows OS
   // when no camera exist. Therefore the FAILED macro can't be used.
+
+  if (hr == S_OK) {
+    // Enumerate all video capture devices.
+    for (ScopedComPtr<IMoniker> moniker;
+        enum_moniker->Next(1, moniker.GetAddressOf(), NULL) == S_OK;
+        moniker.Reset()) {
+      ScopedComPtr<IPropertyBag> prop_bag;
+      hr = moniker->BindToStorage(0, 0, IID_PPV_ARGS(&prop_bag));
+      if (FAILED(hr))
+        continue;
+
+      // Find the description or friendly name.
+      ScopedVariant name;
+      hr = prop_bag->Read(L"Description", name.Receive(), 0);
+      if (FAILED(hr))
+        hr = prop_bag->Read(L"FriendlyName", name.Receive(), 0);
+
+      if (FAILED(hr) || name.type() != VT_BSTR)
+        continue;
+
+      const std::string device_name(base::SysWideToUTF8(V_BSTR(name.ptr())));
+      if (IsDeviceBlackListed(device_name))
+        continue;
+
+      name.Reset();
+      hr = prop_bag->Read(L"DevicePath", name.Receive(), 0);
+      std::string id;
+      if (FAILED(hr) || name.type() != VT_BSTR) {
+        id = device_name;
+      } else {
+        DCHECK_EQ(name.type(), VT_BSTR);
+        id = base::SysWideToUTF8(V_BSTR(name.ptr()));
+      }
+
+      const std::string model_id = GetDeviceModelId(id);
+
+      device_descriptors->emplace_back(device_name, id, model_id,
+          VideoCaptureApi::WIN_DIRECT_SHOW);
+    }
+  }
+
+
+  // for whitelisted filters like bebo-game-capture and capture cards
+  hr = dev_enum->CreateClassEnumerator(CLSID_CQzFilterClassManager,
+      enum_moniker.GetAddressOf(), 0);
+
   if (hr != S_OK)
     return;
 
   // Enumerate all video capture devices.
   for (ScopedComPtr<IMoniker> moniker;
-       enum_moniker->Next(1, moniker.GetAddressOf(), NULL) == S_OK;
-       moniker.Reset()) {
+      enum_moniker->Next(1, moniker.GetAddressOf(), NULL) == S_OK;
+      moniker.Reset()) {
     ScopedComPtr<IPropertyBag> prop_bag;
     hr = moniker->BindToStorage(0, 0, IID_PPV_ARGS(&prop_bag));
     if (FAILED(hr))
@@ -189,7 +259,7 @@ static void GetDeviceDescriptorsDirectShow(Descriptors* device_descriptors) {
       continue;
 
     const std::string device_name(base::SysWideToUTF8(V_BSTR(name.ptr())));
-    if (IsDeviceBlackListed(device_name))
+    if (!IsDeviceWhiteListed(device_name))
       continue;
 
     name.Reset();
@@ -205,7 +275,7 @@ static void GetDeviceDescriptorsDirectShow(Descriptors* device_descriptors) {
     const std::string model_id = GetDeviceModelId(id);
 
     device_descriptors->emplace_back(device_name, id, model_id,
-                                     VideoCaptureApi::WIN_DIRECT_SHOW);
+        VideoCaptureApi::WIN_DIRECT_SHOW);
   }
 }
 
